9bb7c5a0d8cfc8ed95456a8ad53c0a1ac48f5467
Cormac Cureton <cormaccureton@gmail.com>
2020-12-14 21:38:33 -0500

    Link See now button to charts page.

--- src/components/seeNow.js
+++ src/components/seeNow.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Button, Image } from "react-bootstrap";
+import { seeNow } from "../utils/strava";
 import "./seeNow.css";
 
 export default class SeeNow extends Component {
   render() {
     return (
       <div className="seeNow">
-        <Button>
+        <Button onClick={() => seeNow()}>
           See Now
           <Image src="barChart.svg" alt="Bart Chart Icon"></Image>
         </Button>
--- src/utils/strava.js
+++ src/utils/strava.js
@@ -1,22 +1,32 @@
-export  function authSignIn(){
-    console.log('Redirect to Strava login');
-    window.open("https://www.strava.com/oauth/authorize?client_id=53919&response_type=code&redirect_uri=http://localhost:3000/&approval_prompt=force&scope=read", "_self");
+export function authSignIn() {
+  console.log("Redirect to Strava login");
+  window.open(
+    "https://www.strava.com/oauth/authorize?client_id=53919&response_type=code&redirect_uri=http://localhost:3000/select&approval_prompt=force&scope=read",
+    "_self"
+  );
 }
 
-export  function readCodeAndScope(){
-    let response = {};
-    response.error = getUrlParameter('error');
-    if(!response.error){
-        response.code = getUrlParameter('code');
-        response.scope = getUrlParameter('scope');
-    }
-    console.log(response);
-    return response;
+export function seeNow() {
+  console.log("Redirect to Strava login");
+  window.open("http://localhost:3000/charts", "_self");
+}
+
+export function readCodeAndScope() {
+  let response = {};
+  response.error = getUrlParameter("error");
+  if (!response.error) {
+    response.code = getUrlParameter("code");
+    response.scope = getUrlParameter("scope");
+  }
+  console.log(response);
+  return response;
 }
 
 function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(window.location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-};
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+  var results = regex.exec(window.location.search);
+  return results === null
+    ? ""
+    : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
